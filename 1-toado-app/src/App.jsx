90e0bfe3d81d42c0a111017a5d0a4196eef94468
AyeshaNawazKhan <ayeshamnawazkhan1@gmail.com>
2026-07-30 14:25:27 -0700

    Update React components and CSS modules

--- 1-toado-app/src/App.jsx
+++ 1-toado-app/src/App.jsx
@@ -2,19 +2,20 @@ import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItem1 from "./Components/TodoItem1";
 import TodoItem2 from "./Components/TodoItem2";
+import "./App.css";
 
 function App() {
   return (
-
     <center className="todo-container">
-      <div className="container text-center">
+      <div className="container">
         <AppName />
         <AddTodo />
-        <TodoItem1></TodoItem1>
-        <TodoItem2></TodoItem2>
+        <div className="items-container">
+          <TodoItem1></TodoItem1>
+          <TodoItem2></TodoItem2>
+        </div>
       </div>
     </center>
-    
   );
 }
 
--- 1-toado-app/src/Components/TodoItem2.jsx
+++ 1-toado-app/src/Components/TodoItem2.jsx
@@ -1,14 +1,14 @@
 function TodoItem2() {
-  let todoName = "Go To Cpllege";
+  let todoName = "Go To College";
   let todoDate = "7/27/2026";
 
   return (
-    <div className="container text-center">
-      <div className="row">
+    <div className="container">
+      <div className="row kg-row">
         <div className="col-6">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
-          <button type="button" className="btn btn-danger">
+          <button type="button" className="btn btn-danger kg-button">
             Delete
           </button>
         </div>
